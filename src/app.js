7186f77e5260e3e31e636295b9833b9d70e2be4a
Kentros Alexandros <crAlexcomp@gmail.com>
2019-08-08 14:09:41 +0300

    Latest Babel and WebPack version

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
 import React from "react";
-import ReactDom from "react-dom";
- 
+import ReactDOM from "react-dom";
+
+const template = <p> This is JSX FROM WEBPACK </p>; // React.createElement("p", {}, "testing");
+ReactDOM.render(template, document.getElementById("app"));   //
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/app.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
+  },
+  //loader -> let's you customize the behaviour of webpack when it loads a specific file
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
   }
 };
